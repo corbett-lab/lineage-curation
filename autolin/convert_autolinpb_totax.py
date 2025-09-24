c8c9ac9c2d5d5195199cf071d586ae0187d8fedc
lilymaryam <lmkarim@ucdavis.edu>
2025-09-24 14:38:08 -0700

    big update

--- autolin/convert_autolinpb_totax.py
+++ autolin/convert_autolinpb_totax.py
@@ -1,7 +1,9 @@
+import cmd
 import subprocess
 import argparse
 import os 
 import sys
+import gzip
 
 '''
 next iteration of this will likely be a snakemake workflow
@@ -23,11 +25,16 @@ run usher_to_taxonium with --clade_types to convert autolin.pb to taxonium json
 def parse_args():
     parser = argparse.ArgumentParser(description="Convert an AutoLIN protobuf file to a Taxonium JSON file.")
     parser.add_argument("--autolin_pb_path", "-a", type=str, required=True, help="Path to the input AutoLIN protobuf file.")
+    #prob dont need this anymore, unless original Sc2 tree is used
     parser.add_argument("--sars-cov-2", "-sc2", action="store_true", help="Flag indicating if the data is SARS-CoV-2. Special \
         options must be handled for SC2")
+    parser.add_argument("--additional_meta_data", "-amd", type=str, required=False, help="Path to tab separated metadata file, if additional metadata is desired. Sample ID column MUST be first.")
+    #save this for later. make sure alex is changing the name of the column in taxonium
+    #parser.add_argument("--rename_annotation_column", "-o", type=str, required=False, help="Path to the output Taxonium JSON file. If not provided, \
+    #    the output will be saved in the same directory as the input AutoLIN protobuf file with a .jsonl.gz extension.")
     return parser.parse_args()
 
-def convert_autolinpb_totax(autolin_pb_path, parent_dir):
+def get_annotations(autolin_pb_path, parent_dir):
     """
     Convert an AutoLIN protobuf file to a Taxonium JSON file.
 
@@ -52,7 +59,7 @@ def fix_metadata(clade_file):
     command = "sed -i '1s/^[^ \t]*/strain/' "+clade_file
     subprocess.run(command, shell=True, check=True)
 
-def usher_to_taxonium(autolin_pb_path, clade_file, sc2):
+def usher_to_taxonium(autolin_pb_path, clade_file, sc2, columns):
     #this is a crude version that doesnt have any option for column name changing 
     #next iteration will need to read column names so im not hardcoding them 
     #will deal with this in next iteration
@@ -61,20 +68,78 @@ def usher_to_taxonium(autolin_pb_path, clade_file, sc2):
         #command= ["usher_to_taxonium","-i", autolin_pb_path, "--clade_types", "nextclade,pango", "-m", clade_file, "-c", "strain,annotation_1,annotation_2", "-o", autolin_pb_path.replace(".pb", ".jsonl.gz")]
         print("Currently, SARS-CoV-2 is unsupported. Check back in later releases. Exiting.", file=sys.stderr)
         sys.exit(1)
-    
-    command= ["usher_to_taxonium","-i", autolin_pb_path, "--clade_types", "pango", "-m", clade_file, "-c", "strain,annotation_1", "-o", autolin_pb_path.replace(".pb", ".jsonl.gz")]
+
+    command= ["usher_to_taxonium","-i", autolin_pb_path, "--clade_types", "pango", "-m", "phenometa.tsv", "-c", ",".join(columns), "-o", autolin_pb_path.replace(".pb", ".jsonl.gz")]
     #print(command)
     subprocess.run(command, check=True)
 
+def is_gzipped(filepath):
+    with open(filepath, 'rb') as f:
+        return f.read(2) == b'\x1f\x8b'
 
+#one avenue to handle this. 
+#i think i prefer a join command 
+#need to figure out what docker supports for gzip
+'''
+def read_metadata(file, clade_file):
+    header = file.readline().strip()
+    print(header)
+    assert '\t' in header, f"Error: metadata is not tab-separated."
+    print('metadata is tab separated')
+    header.append
+    
+
+
+def merge_meta(amd, clade_file):
+    print('HERE')
+    if is_gzipped(amd):
+        with gzip.open(amd, 'rt') as f:
+            read_metadata(f, clade_file)
+    else:
+        with open(amd, 'r') as f:
+            read_metadata(f, clade_file)
+'''
+
+#THIS CODE BLOCK IS NOT FINAL
+#MIXING PYTHON AND BASH IS NOT PREFERRED
+#WILL UPDATE SOON
+#note these commands use bash specific syntax
+#this may not work in all environments
+#i think this is ok for now since taxonium and usher_to_taxonium are also bash
+#need to check with docker container
+#all of this will likely be refactored into a snakemake workflow later
+def merge_meta(amd, clade_file):
+    with open(clade_file, 'r') as f:
+        header = f.readline().strip().split('\t')
+    #this ensures that all files have one annotation per node.
+    #maybe change later if there is a need 
+    assert len(header) == 2, f"Error: {clade_file} does not have exactly 2 columns."
+    if is_gzipped(amd):
+        cmd1 = f"echo \"$(head -n1 {clade_file})\\t$(zcat {amd} | head -n1 | cut -f2-)\" > phenometa.tsv"
+        cmd2 = f"join -t $'\\t' <(tail -n +2 {clade_file} | sort -k1,1) <(zcat {amd} | tail -n +2 | sort -k1,1) >> phenometa.tsv"
+    else:
+        cmd1 = f"echo \"$(head -n1 {clade_file})\\t$(head -n1 {amd} | cut -f2-)\" > phenometa.tsv"
+        cmd2 = f"join -t $'\\t' <(tail -n +2 {clade_file} | sort -k1,1) <(tail -n +2 {amd} | sort -k1,1) >> phenometa.tsv"
+    subprocess.run(cmd1, shell=True, check=True)
+    subprocess.run(cmd2, shell=True, check=True, executable="/bin/bash")
+    with open('phenometa.tsv', 'r') as f:
+        columns = f.readline().strip().split('\t')
+    return columns
+
+#this currently assumes that there is only 1 annotation within the mat
+#it will break if there are more than 1
 def main(): 
     args = parse_args()
     autolin_pb_path = args.autolin_pb_path
+    amd = None
+    if args.additional_meta_data:
+        amd = args.additional_meta_data
+    #make sure directory handling is consistent between scripts
     parent_dir = os.path.dirname(os.path.abspath(autolin_pb_path))
     sc2 = args.sars_cov_2
     #print(parent_dir)
     print(autolin_pb_path)
-    convert_autolinpb_totax(autolin_pb_path, parent_dir)
+    get_annotations(autolin_pb_path, parent_dir)
     #make sure autolin_clade.tsv is not empty
     clade_file = None
 
@@ -86,7 +151,16 @@ def main():
             clade_file = parent_dir+"/autolin_clade.tsv"
             print('clade', clade_file)
             fix_metadata(clade_file)
-    usher_to_taxonium(autolin_pb_path, clade_file, sc2)
+            if amd != None:
+                columns = merge_meta(amd, clade_file)
+            else:
+                subprocess.run(f"mv {clade_file} phenometa.tsv", shell=True, check=True)
+                with open('phenometa.tsv', 'r') as f:
+                    header = f.readline().strip().split('\t')
+                    assert len(header) == 2, f"Error: {clade_file} does not have exactly 2 columns."
+
+                    columns = f.readline().strip().split('\t')
+    usher_to_taxonium(autolin_pb_path, clade_file, sc2, columns)
 
 if __name__ == "__main__":
     main()
--- autolin/phenotypes.py
+++ autolin/phenotypes.py
@@ -3,6 +3,8 @@ import argparse
 import subprocess
 import gzip
 
+'''
+'''
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Process phenotype data.")
@@ -11,7 +13,7 @@ def parse_args():
         '-m',
         type=str,
         required=True,
-        help="Path to the input metadata file.",
+        help="Path to the input metadata file. Note: this script currently expects a tab-delimited file with a header line. Other formats may not work correctly.",
     )
     parser.add_argument(
         "--mat",
@@ -25,8 +27,16 @@ def parse_args():
         "-c",
         type=str,
         required=True,
-        help="Name of the column to extract from the metadata file",
+        help="Name of the column to extract from the metadata file. NOTE: Must exactly match the header name in the metadata file.",
     )
+    '''
+    parser.add_argument(
+        "--run_autolin",
+        "-r",
+        action='store_true',
+        help="If flagged, autolin will run from this script with the -p flag and generated weights ",
+    )
+    '''
     parser.add_argument(
         "--output_file", 
         '-o',
@@ -46,85 +56,107 @@ def get_samples(tree):
     os.remove("samples.subprocess")
     return samples
 
-def read_meta(file, samples):
-    print(samples)
+#maybe this should be a join command? 
+#not sure 
+def read_meta(file, samples, column_name):
     metadata = {}
     headers = file.readline().strip().split('\t')
+    print(headers)
+    if column_name not in headers:
+        raise ValueError(f"Column '{column_name}' not found in metadata file headers.")
+    col_index = headers.index(column_name)
     for line in file:
-        parts = line.strip().split()
+        parts = line.strip('\n').split('\t')
         sample_id = parts[0]
         #print(sample_id)
         if sample_id in samples:
-            metadata[sample_id] = {headers[i]: parts[i] for i in range(1, len(parts))}
-    print(metadata)
+            #i think a subdict it less useful
+            metadata[sample_id] = parts[col_index]
+            #metadata[sample_id] = {headers[col_index]: parts[col_index]}
+    #print(metadata)
     return metadata
 
-def get_metadata(samples, metadata_file):
+def get_metadata(samples, metadata_file, column_name):
     if metadata_file.endswith(".gz"):
         with gzip.open(metadata_file, "rt") as gz:
-            metadata = read_meta(gz, samples)
-            '''
-            for line in gz:
-                parts = line.strip().split(',')
-                sample_id = parts[0]
-                if sample_id in samples:
-                    metadata[sample_id] = {headers[i]: parts[i] for i in range(1, len(parts))}
-            '''
+            metadata = read_meta(gz, samples, column_name)
     else:
         with open(metadata_file, 'r') as f:
-            metadata = read_meta(f, samples)
-            '''
-            for line in f:
-                parts = line.strip().split(',')
-                sample_id = parts[0]
-                if sample_id in samples:
-                    metadata[sample_id] = {headers[i]: parts[i] for i in range(1, len(parts))}
-            '''
-    return metadata
-    ''' 
-    with gzip.open(metadata_file, 'r') as f:
-        headers = f.readline().strip().split(',')
-        for line in f:
-            parts = line.strip().split(',')
-            sample_id = parts[0]
-            if sample_id in samples:
-                metadata[sample_id] = {headers[i]: parts[i] for i in range(1, len(parts))}
+            metadata = read_meta(f, samples, column_name)
     return metadata
 
-'''
+def frequency_based_weight(metadata, output):
 
-'''
-def get_metadata(metadata_file):
-    # Sort samples and metadata
-    subprocess.run("sort samples.subprocess > samples.sorted", shell=True, check=True)
-    subprocess.run(f"sort -t, -k1,1 {metadata_file} > metadata.sorted", shell=True, check=True)
-    # Join sorted files
-    subprocess.run("join -t, samples.sorted metadata.sorted > matched_metadata.csv", shell=True, check=True)
-    
-    # Read matched metadata
-    metadata = {}
-    with open("matched_metadata.csv", "r") as f:
-        for line in f:
-            parts = line.strip().split(",")
-            sample_id = parts[0]
-            metadata[sample_id] = parts[1:]
-    # Optionally, clean up temp files
-    # os.remove("samples.subprocess")
-    # os.remove("samples.sorted")
-    # os.remove("metadata.sorted")
-    # os.remove("matched_metadata.csv")
-    
-    #return metadata
-'''
+    counts = {}
+    for p in metadata.values():
+        if p in counts:
+            counts[p] += 1
+        else:
+            counts[p] = 1
 
+    total = sum(counts.values())
+
+    # Compute frequency-based weights (rarer = higher weight)
+    weights = {}
+    for sample, p in metadata.items():
+        freq = counts[p] / total
+        weights[sample] = 1 - freq
+    with open(output, 'w') as f:
+        f.write("Sample\tWeight\tPhenotype\n")
+        for sample in metadata:
+            f.write(f"{sample}\t{weights[sample]:.4f}\t{metadata[sample]}\n")
+            #f.write((f"{sample}\t{weights[sample]:.4f}\n"))
+
+#will change this to make it more general later 
+def ordinal_based_weight(metadata, output):
+    categories = ["Sensitive", "HR-TB", "RR-TB", "MDR-TB", "Pre-XDR-TB", "XDR-TB"]
+    n = len(categories)
+    weights = {cat: (i+1)/n for i, cat in enumerate(categories)}
+    
+
+    # Compute frequency-based weights (rarer = higher weight)
+    #for sample, p in metadata.items():
+    #    metadata[sample] = weights[p]
+    #    weights[sample] = 1 - freq
+    weight = 0.0
+    with open(output, 'w') as f:
+        f.write("Sample\tWeight\tPhenotype\n")
+        for sample in metadata:
+            if metadata[sample] not in weights:
+                weight = 0.5 #default weight if not in categories
+            else:
+                weight = weights[metadata[sample]]
+            f.write(f"{sample}\t{weight:.4f}\t{metadata[sample]}\n")
+            #f.write((f"{sample}\t{weights[sample]:.4f}\n"))
+
+def format_outputs(output_file):
+    cmd = f"cut -f1,3 {output_file} > pheno.pheno.tsv"
+    subprocess.run(cmd, shell=True, capture_output=True, text=True)
+    cmd = f"cut -f1,2 {output_file} > pheno.weights.tsv"
+    subprocess.run(cmd, shell=True, capture_output=True, text=True)
+    #this may cause local issues
+    #cmd = f"python3 propose_sublineages.py -i {args.mat} -p pheno.weights.tsv -o {args.mat.replace('.pb', '.pheno.autolin.pb')}"
+    #run these within convert ?
+    #cmd = f"echo -e \"$(head -n1 autolin_clade.tsv)\t$(head -n1 pheno.pheno.tsv | cut -f2-)\" > phenometa.tsv"
+    #subprocess.run(cmd, shell=True, capture_output=True, text=True)
+    #cmd = f"join -t $'\t'     <(tail -n +2 autolin_clade.tsv | sort -k1,1)     <(tail -n +2 pheno.pheno.tsv | sort -k1,1) >> phenometa.tsv"
+    #subprocess.run(cmd, shell=True, capture_output=True, text=True)
 
 def main():
     args = parse_args()
     tree = args.mat
     samples = get_samples(tree)
     print(f"Extracted {len(samples)} samples from the tree.")
-    meta = get_metadata(samples, args.metadata_file)
+    meta = get_metadata(samples, args.metadata_file, args.column_name)
     print(len(meta))
+
+    #try frequency based 
+    #frequency_based_weight(meta, args.output_file)
+    #ordinal based
+    ordinal_based_weight(meta, args.output_file)
+    format_outputs(args.output_file)
+    print("Weights and phenotypes written to pheno.weights.tsv and pheno.pheno.tsv respectively.")
+    
     '''
     # Read metadata file
     with open(args.metadata_file, 'r') as f:
